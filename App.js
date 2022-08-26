e9913566ae05aaf9f92bc265fbee41fa24ad8a4c
tathevve <tathev.khachatryan@gmail.com>
2022-08-26 15:50:07 +0400

    lesson-10

--- App.js
+++ App.js
@@ -12,47 +12,100 @@ import {
   Text,
   Button,
   View,
+  ScrollView,
+  RefreshControl,
+  FlatList,
+  SectionList,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 
 const App = () => {
 
+  const [items, setItems] = useState([
+    { name: 'Item 1' },
+    { name: 'Item 2' },
+    { name: 'Item 3' },
+    { name: 'Item 4' },
+    { name: 'Item 5' },
+    { name: 'Item 6' },
+    { name: 'Item 7' },
+    { name: 'Item 8' },
+  ])
+
+  const datas = [
+    {
+      title: 'title 1',
+      data: ['item 1-1', 'item 1-2', 'item 1-3']
+    },
+    {
+      title: 'title 2',
+      data: ['item 2-1', 'item 2-2', 'item 2-3']
+    },
+    {
+      title: 'title 3',
+      data: ['item 3-1']
+    },
+    {
+      title: 'title 4',
+      data: ['item 4-1', 'item 4-2']
+    },
+  ]
+
+  const [refreshing, setRefreshing] = useState(false)
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setItems([...items, { key: '69', name: 'item 69' }]);
+    setRefreshing(false)
+  }
+
   return (
-    <View style={styles.body}>
-      <View style={styles.firstRow}>
-        <View style={styles.view1}>
-          <Text style={styles.text}>1</Text>
+    <SectionList
+      keyExtractor={(item, index) => index.toString()}
+      sections={datas}
+      renderItem={({ item }) => (
+          <Text style={styles.text}>{item}</Text>
+      )}
+      renderSectionHeader={({ section }) => (
+        <View style={styles.item} >
+          <Text style={styles.text}>{section.title}</Text>
         </View>
-        <View style={styles.view2}>
-          <Text style={styles.text}>2</Text>
-        </View>
-        <View style={styles.view3}>
-          <Text style={styles.text}>3</Text>
-        </View>
-      </View>
-      <View style={styles.view4}>
-        <Text style={styles.text}>4</Text>
-      </View>
-      <View style={styles.view5}>
-        <Text style={styles.text}>5</Text>
-      </View>
-      <View style={styles.row2}>
-        <View style={styles.view6}>
-          <Text style={styles.text}>6</Text>
-        </View>
-        <View style={styles.view7}>
-          <Text style={styles.text}>7</Text>
-        </View>
-      </View>
-    </View>
+      )}
+    />
+    // <FlatList
+    //   keyExtractor={(item, index) => index.toString()}
+    //   data={items}
+    //   renderItem={ ({item}) =>
+    //     <View style={styles.item} >
+    //       <Text style={styles.text}>{item.name}</Text>
+    //     </View>
+    //   }
+    //     refreshControl={
+    //     <RefreshControl
+    //       refreshing={refreshing}
+    //       onRefresh={onRefresh}
+    //       colors ={['#ff00ff']}
+    //     />}
+    // />
+    // <ScrollView
+    //   style={styles.body}
+    //   refreshControl={
+    //     <RefreshControl
+    //       refreshing={refreshing}
+    //       onRefresh={onRefresh}
+    //       colors ={['#ff00ff']}
+    //     />}
+    // >
+    //   {
+    //     items.map((object) => {
+    //       return (
+    //         <View style={styles.item} key={object.key}>
+    //           <Text style={styles.text}>{object.item}</Text>
+    //         </View>
+    //       )
+    //     })
+    //   }
+    // </ScrollView>
   );
 };
 
@@ -60,60 +113,16 @@ const styles = StyleSheet.create({
   body: {
     flex: 1,
     backgroundColor: 'white',
-    alignItems: 'stretch',
+  },
+  item: {
+    backgroundColor: '#AAA',
     justifyContent: 'center',
-  },
-  firstRow: {
-    display: 'flex',
-    flexDirection: 'row',
-
-  },
-  view1: {
-    flex: 1,
-    backgroundColor: '#CCC',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view2: {
-    flex: 2,
-    backgroundColor: '#CBC',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view3: {
-    flex: 3,
-    backgroundColor: '#CCA',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view4: {
-    backgroundColor: '#CAA',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view5: {
-    backgroundColor: '#CAC',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  row2:{
-    display:'flex',
-    flexDirection:'row',
-    alignItems:'stretch',
-    justifyContent:'center'
-  },  
-  view6: {
-    backgroundColor: '#AAF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view7: {
-    backgroundColor: '#CAB',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 10
   },
   text: {
-    fontSize: 40
+    fontSize: 40,
+    margin: 10
   },
 });
 
